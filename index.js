537190653e23a379a7e0cfa7c113b6257cd21daa
iqqx <sos>
2024-09-16 09:42:48 +0300

    2

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/faq", (req, res) => {
     res.render("faq", {age: "15 лет"})
 })
 
+app.get("/blogs", (req, res) => {
+    res.render("blogs", {})
+})
+
 app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
 });
