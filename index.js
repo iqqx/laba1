194193efb7e9df0ee98e8331cbbf35a56fd0e22f
lifhr <lifhr>
2024-09-19 10:33:02 +0300

    половина 3

--- index.js
+++ index.js
@@ -14,22 +14,49 @@ const requestTime = (req, res, next) => {
 }
 
 app.use(requestTime)
-
 app.use(myLogger)
 
-app.use("/static", __dirname + express.static("public"))
+app.use("/static", express.static(__dirname + "/public"))
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send("Something broke!")
 })
 
-app.get("/faq", (req, res) => {
+app.get("/", (req, res) => {
+    res.render("main")
+})
+
+app.get("/faq", (req, res) =>  {
     res.render("faq", { age: "15 лет" })
 })
 
 app.get("/blogs", (req, res) => {
     res.render("blogs", {})
+})
+
+const todo = [
+    {
+        Id: 0,
+        Title: "Задача 1",
+        Completed: false
+    },
+    {
+        Id: 1,
+        Title: "Задача 2",
+        Completed: true
+    }
+]
+
+app.get("/todo", (req, res) => {
+    res.render("todolist", {
+        todolist: todo
+    })
+})
+
+app.post("/todo/complete", (req, res) => {
+    // todo.find((todo) => todo.Id === )
+    res.redirect("/todo/");
 })
 
 app.post("/", (req, res) => {
